446539234fc870d28af25025bc94dca0b41a5f88
MaYank <190871930+M4y4nky@users.noreply.github.com>
2026-02-13 23:08:40 +0530

    Delete vehicle-info directory

--- vehicle-info/script.js
+++
@@ -1,9 +0,0 @@
-fetch("https://m4y4nky-vehicle-api.vercel.app/api/vehicle", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    vehicle_no: vehicleNo
-  })
-});
